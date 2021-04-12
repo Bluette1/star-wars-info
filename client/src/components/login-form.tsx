65421e82be64341c616112f917fba4c8876dd1d7
Bluette1 <marylene.sawyer@gmail.com>
2021-04-12 14:07:48 +0200

    Fix the login and register forms

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik';
+import React, { Fragment } from 'react';
 import login from '../login';
 import RegisterButton from '../register-button';
 
@@ -10,48 +7,34 @@ export default function LoginForm() {
     <div className="row d-flex justify-content-center form mt-5 pb-5">
       <div className="col-md-3">
         <h4>Login</h4>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validate={(values) => {
-            const errors = { email: '' };
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              login(values);
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form className="d-flex flex-column">
-              <Field type="email" name="email" className="mb-3 form-elements" />
-              <ErrorMessage name="email" component="div" />
-              <Field
-                type="password"
-                name="password"
-                className="mb-3 mt-3 form-elements"
-              />
-              <ErrorMessage name="password" component="div" />
-              <div>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="form-elements"
-                >
-                  Submit
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+
+        <>
+          <form id="login" className="d-flex flex-column">
+            <label htmlFor="email" className="row d-flex flex-column">
+              Email:
+              <input className="col-md-10" type="email" name="email" id="email" />
+            </label>
+            <label htmlFor="password" className="row mb-3 mt-3">
+              Password:
+              <input className="col-md-10" type="password" name="password" id="password" />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              className="form-elements btn btn-secondary"
+              onClick={(e) => {
+                e.preventDefault();
+                const email = document.getElementById(
+                  'email',
+                ) as HTMLInputElement;
+                const password = document.getElementById(
+                  'password',
+                ) as HTMLInputElement;
+                login({ email, password });
+              }}
+            />
+          </form>
+        </>
         <RegisterButton />
       </div>
     </div>
--- client/src/components/register-form.tsx
+++ client/src/components/register-form.tsx
@@ -1,64 +1,46 @@
 import React from 'react';
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik';
-import login from '../login';
+import register from '../register';
 
 export default function RegisterForm() {
   return (
     <div className="row d-flex justify-content-center form mt-5 pb-5">
       <div className="col-md-3">
-        <h4>Register</h4>
-        <Formik
-          initialValues={{ email: '', password: '', passwordConfirmation: '' }}
-          validate={(values) => {
-            const errors = { email: '', password: '' };
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            } else if (values.password !== values.passwordConfirmation) {
-              errors.password = 'Invalid password';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              login(values);
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form className="d-flex flex-column">
-              <Field type="email" name="email" className="mb-3 form-elements" />
-              <ErrorMessage name="email" component="div" />
-              <Field
-                type="password"
-                name="password"
-                className="mb-3 mt-3 form-elements"
-              />
-              <ErrorMessage name="password" component="div" />
-              <Field
-                type="password"
-                name="password-confirmation"
-                className="mb-3 mt-3 form-elements"
-              />
-              <ErrorMessage name="passwordConfirmation" component="div" />
-              <div>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="form-elements"
-                >
-                  Submit
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        <h4>Login</h4>
+
+        <>
+          <form id="login" className="d-flex flex-column">
+            <label htmlFor="email" className="row d-flex flex-column">
+              Email:
+              <input className="col-md-10" type="email" name="email" id="email" />
+            </label>
+            <label htmlFor="password" className="row mb-3 mt-3">
+              Password:
+              <input className="col-md-10" type="password" name="password" id="password" />
+            </label>
+            <label htmlFor="password" className="row mb-3 mt-3">
+              Name:
+              <input className="col-md-10" type="text" name="name" id="name" />
+            </label>
+            <input
+              type="submit"
+              value="Submit"
+              className="form-elements btn btn-secondary"
+              onClick={(e) => {
+                e.preventDefault();
+                const email = document.getElementById(
+                  'email',
+                ) as HTMLInputElement;
+                const password = document.getElementById(
+                  'password',
+                ) as HTMLInputElement;
+                const name = document.getElementById(
+                  'name',
+                ) as HTMLInputElement;
+                register({ email: email.value, password: password.value, name: name.value });
+              }}
+            />
+          </form>
+        </>
       </div>
     </div>
   );
